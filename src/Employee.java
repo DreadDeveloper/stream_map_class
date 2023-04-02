55778c966e4ab9ed6053ee45886f11286464615e
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-02 18:33:11 +0530

    Initial Commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee
+{
+	private final int empNo;
+	private final String empName;
+	public float empSalary;
+	Employee(int empNo, String empName, float empSalary)
+	{
+		this.empNo = empNo;
+		this.empName = empName;
+		this.empSalary = empSalary;
+	}
+	public int getEmpNo()
+	{
+		return empNo;
+	}
+	public String getEmpName()
+	{
+		return empName;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		List<Employee> employeeList = new ArrayList<>();
+		employeeList.add(new Employee(1, "John Doe", 56000.0f));
+		employeeList.add(new Employee(2, "Jane Doe", 60000.0f));
+		employeeList.add(new Employee(3, "Joe Mama", 46000.0f));
+		employeeList.add(new Employee(4, "Edward", 30000.0f));
+		employeeList.add(new Employee(5, "Ludwig", 95000.0f));
+		System.out.print("Enter the employee salary bonus: ");
+		float bonus = scanner.nextFloat();
+		List<Float> revisedSalaryList = employeeList.stream().map(n -> n.empSalary = n.empSalary + bonus).collect(Collectors.toList());
+		int n = 0;
+		for(Employee e : employeeList)
+		{
+			e.empSalary = revisedSalaryList.get(n);
+			n++;
+			System.out.println(e.getEmpNo() + " " + e.getEmpName() + " " + e.empSalary);
+		}
+	}
 }
